badedd6d2ea90a7e808bc70a460e1bb2e6c1915e
Lord-HaHaHa <57814183+Lord-HaHaHa@users.noreply.github.com>
2022-11-18 13:16:12 +0100

    Simple Menu Page + SQL DB and Query

--- nodeapp/routes/menu.js
+++ nodeapp/routes/menu.js
@@ -3,11 +3,25 @@ var router = express.Router();
 var db = require('../database');
 
 /* GET menu listing. */
-router.get('/menu', function(req, res, next) {
-  db.pool.getConnection().then(conn =>{
-    console.log(conn)
+router.get('/', function(req, res, next) {
+  
+  var siteData = []
+
+  // SQL Connection and Querys
+  db.pool.getConnection().then(conn => {
+    let sqlcmd = 'SELECT name, img, price FROM Menu WHERE restaurantID = 1'
+    conn.query(sqlcmd).then(rows => {
+      // Generate the siteData
+      rows.forEach(element => {
+        console.log(element.name)
+        var imgBuffer = new Buffer(element.img).toString("base64") // Conver BLOB to binary64
+        siteData.push({name: element.name, img: imgBuffer, price: element.price})
+      });
+
+      // Start Loading page
+      res.render('menu', { title: 'Menu', data: siteData });
+    })
   })
-  res.render('menu', { title: 'Menu' });
 });
 
 module.exports = router;
